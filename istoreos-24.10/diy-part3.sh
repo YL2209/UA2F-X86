18d3c739c5a1fe37d68773cf5b7ee661948a87b1
YL2209 <59274897+YL2209@users.noreply.github.com>
2026-01-07 18:07:46 +0800

    Create diy-part3.sh for OpenWrt setup
    Add OpenWrt DIY script for updating feeds.

---
+++ istoreos-24.10/diy-part3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part3.sh
+# Description: OpenWrt DIY script part 3 (Before Update feeds)
+#
+
+mv openwrt-24.10/src/banner openwrt/package/base-files/files/etc/banner
+# mv openwrt-24.10/src/img/bg1.webp openwrt/package/feeds/luci/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
