4892b7b7156f2f8e64f370a019a1bf2cd7454529
YL2209 <59274897+YL2209@users.noreply.github.com>
2026-01-07 18:37:23 +0800

    Update diy-part3.sh

--- istoreos-24.10/diy-part3.sh
+++ istoreos-24.10/diy-part3.sh
@@ -10,5 +10,5 @@
 # Description: OpenWrt DIY script part 3 (Before Update feeds)
 #
 
-mv openwrt-24.10/src/banner openwrt/package/base-files/files/etc/banner
-# mv openwrt-24.10/src/img/bg1.webp openwrt/package/feeds/luci/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+mv istoreos-24.10/src/banner openwrt/package/base-files/files/etc/banner
+# mv istoreos-24.10/src/img/bg1.webp openwrt/package/feeds/luci/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
